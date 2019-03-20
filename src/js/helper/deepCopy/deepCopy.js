eb860d9ee683d17fc5ae93991b725b92ad621ca3
Shriram <shriram1897@gmail.com>
2019-03-20 14:46:06 +0530

    Array.isArray() and replaced isObject with typeof

--- src/js/helper/deepCopy/deepCopy.js
+++ src/js/helper/deepCopy/deepCopy.js
@@ -1,13 +1,13 @@
-import { isArray, isObject, isDate } from 'util';
+import { isDate } from 'util';
 
 export function deepCopy(element) {
-    if (isArray(element)) {
+    if (Array.isArray(element)) {
         return element.map(el => {
             return deepCopy(el);
         });
     } else if (isDate(element)) {
         return new Date(element);
-    } else if (isObject(element)) {
+    } else if (element !== null && typeof element === 'object') {
         let newObect = {};
         for (let key in element) {
             newObect[key] = deepCopy(element[key])
@@ -17,3 +17,4 @@ export function deepCopy(element) {
         return element;
     }
 }
+window.deepCopy = deepCopy;
